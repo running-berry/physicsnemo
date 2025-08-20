267433448a1e1c3a412c206e873442296a381c3e
CH Lin <7hong0606@gmail.com>
2025-08-20 23:22:46 +0800

    fix: updated 1N Slurm script

--- dev/scripts/1N.sh
+++ dev/scripts/1N.sh
@@ -5,22 +5,21 @@
 #SBATCH -p <PARTITION_NAME>      # need partition name
 #SBATCH -o %x.%j.out           # output log
 #SBATCH -e %x.%j.err           # error log
-#SBATCH --gpus-per-node=1      # number of GPU per node
-# job runtime limit
-#SBATCH -t 01:00:00
+#SBATCH --gpus-per-node=1      # number of GPUs per node
+#SBATCH -t 01:00:00            # job runtime limit
 
 HOMEDIR=<HOME_DIR>  # replace with actual home directory
 SQSH=<SQSH_FILE>  # replace with actual SQSH file
 PARTITION=<PARTITION_NAME>  # replace with actual partition name
 
-echo -e "Running on hosts: $(echo $(scontrol show hostname))"
+echo -e "Running on hosts: $(scontrol show hostname)"
 
-srun -N 1 -p ${PARTITION}  --mpi=pmix --gres=gpu:1 \
---container-image ${HOMEDIR}${SQSH} \
---container-writable \
---container-remap-root \
---container-mounts ${HOMEDIR}/projects/physicsnemo:/workspace \
-bash -c "
-cd dev
-# make run
-"
+srun -N 1 -p ${PARTITION} --mpi=pmix --gres=gpu:1 \
+  --container-image ${HOMEDIR}${SQSH} \
+  --container-writable \
+  --container-remap-root \
+  --container-mounts ${HOMEDIR}:/workspace \
+  bash -c "
+    cd projects/physicsnemo/dev
+    make run
+  "
